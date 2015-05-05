2e23cea389f7b8f6b845dd0a5a5cc4053c94c0e4
Paul Dufour <paul.t.dufour@gmail.com>
2015-05-05 13:53:04 -0700

    add basic tests

---
+++ test/test.js
@@ -0,0 +1,55 @@
+// Settings
+var browser = (typeof(window) !== 'undefined') ? true : false,
+    root = browser ? window : global,
+    nodeSassGlobbing = require('../index.js'),
+    sass = require('node-sass'),
+    path = require('path'),
+    chai = require('chai'),
+    expect = chai.expect,
+    FIXTURES_DIR = path.join(__dirname, 'fixtures');
+
+describe('simple', function() {
+  it('should import globs using scss syntax', function(done) {
+    var inputFile = path.join(FIXTURES_DIR, 'simple_scss/input.scss')
+    var outputFile = path.join(FIXTURES_DIR, 'simple_scss/output.css')
+
+    var callback = function(err,result) {
+      var fs = require('fs')
+
+      fs.readFile(outputFile, function(err, data) {
+        expect(result.css.toString()).to.equal(data.toString())
+        done()
+      })
+    };
+
+    sass.render({
+      file: inputFile,
+      importer: nodeSassGlobbing,
+      success: callback.bind(null, null),
+      error: callback.bind(null),
+      outputStyle: 'compressed'
+    }, callback)
+  })
+
+  it('should import globs using indented syntax', function(done) {
+    var inputFile = path.join(FIXTURES_DIR, 'simple_sass/input.sass')
+    var outputFile = path.join(FIXTURES_DIR, 'simple_sass/output.css')
+
+    var callback = function(err,result) {
+      var fs = require('fs')
+
+      fs.readFile(outputFile, function(err, data) {
+        expect(result.css.toString()).to.equal(data.toString())
+        done()
+      })
+    };
+
+    sass.render({
+      file: inputFile,
+      importer: nodeSassGlobbing,
+      success: callback.bind(null, null),
+      error: callback.bind(null),
+      outputStyle: 'compressed'
+    }, callback)
+  })
+});
