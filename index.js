6419e0d5bef2dffc38728fb4d318342a70bddf1f
Paul Dufour <paul.t.dufour@gmail.com>
2015-05-05 13:53:28 -0700

    filter out self, non-sass files, and return void instead of cb

--- index.js
+++ index.js
@@ -15,27 +15,39 @@ module.exports = function(url, prev, done) {
         prev_dir = path.dirname(matches[1])
         url = url.replace(prev_re, '')
         url = path.resolve(prev_dir, url)
-        return done({file: url})
+        done({file: url})
+        return void 0;
       }
     } else {
-      return done({file: url})
+      done({file: url})
+      return void 0;
     }
   }
 
+  // search for scss | sass files if no extension provided
+
   // url is a glob, like foo/*.sass, resolve to importing all matching files, so
   // if there is foo/1.sass and foo/2.sass, it will import both of those files
-  glob(url, {cwd: path.dirname(prev)}, function (err, files) {
+  var cwd = path.dirname(prev)
+  glob(url, {cwd: cwd}, function (err, files) {
     var ret = {
       contents: "\n"
     }
 
+    // import all the matched files, passing through the current file. this is
+    // because if you didn't, the path will resolve relative to foo/*.sass,
+    // which will fail since that is not an actual path.
     files.forEach(function(file) {
-      // import all the matched files, passing through the current file. this is
-      // because if you didn't, the path will resolve relative to foo/*.sass,
-      // which will fail since that is not an actual path.
-      ret.contents += '@import "|prev=' + prev + '|' + file + "\";\n"
+      var ext = path.extname(file)
+      var abspath = path.resolve(cwd, file)
+
+      if(/^\.s[ac]ss$/.test(ext) && prev !== abspath) {
+        // don't import self
+        var prev_kwd = '|prev=' + prev + '|'
+        ret.contents += '@import "' + prev_kwd + file + "\";\n"
+      }
     })
 
-    return done(ret)
+    done(ret)
   })
 };
